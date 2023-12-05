c940d424d0469806247e9986270730a272f81d38
Sol <55402349+popsitos@users.noreply.github.com>
2023-12-05 11:32:49 -0300

    Update frust.py
    agregue para la salida para SSdraw

--- frust.py
+++ frust.py
@@ -1,5 +1,5 @@
 #Voy a calcular el zscore de cada posicion para relacionar con la frustracion.
-def frust(df,metodo):
+def frust(df,metodo,ss=None): #por si quiero usar el SSdraw
     n=df["n"].unique().tolist()
     for i in range(len(n)):
         df_c=df[df["n"]==n[i]]
@@ -8,4 +8,6 @@ def frust(df,metodo):
         zscore=(df_c[metodo]-prom)/sd
         df.loc[df["n"] == n[i], "zscore"] = zscore.tolist()
     zscore=df[df["aa"]==df["wt"]]
+    if ss != None:
+        zscore["zscore"]=zscore["wt"].astype(str)+" "+zscore["zscore"].astype(str)
     zscore["zscore"].to_csv("/home/solcanale/calculos nuevos con alphafold/Noviembre 2023/frustracion/"+metodo+"_frust.txt",sep="\t",index=False,header=None)
